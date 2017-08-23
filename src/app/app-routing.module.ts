93d33701b5abfa61e9a611689ca2df467080b389
lait <287541828@qq.com>
2017-08-23 16:47:05 +0800

    第一次提交项目 这个是router项目 用来练习angular路由的

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { ProductComponent } from "./product/product.component";
+import { Code404Component } from "./code404/code404.component";
+import { ProductDescComponent } from "./product-desc/product-desc.component";
+import { ProductSellComponent } from "./product-sell/product-sell.component";
+
+const routes: Routes = [
+    { path: '', redirectTo: '/product', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent, },
+    {
+        path: 'product/:id', component: ProductComponent, children:
+        [
+            { path: '', component: ProductDescComponent, },
+            { path: 'desc', component: ProductDescComponent, },
+            { path: 'sell', component: ProductSellComponent, },
+        ]
+    },
+    { path: '**', component: Code404Component, },
+];
+
+@NgModule( {
+    imports: [RouterModule.forRoot( routes )],
+    exports: [RouterModule],
+} )
+export class AppRoutingModule {
+}
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from "@angular/router";
+
+@Component( {
+    selector: 'app-product',
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.css'],
+} )
+export class ProductComponent implements OnInit {
+
+    private productId: number;
+
+    constructor( private rooterInfo: ActivatedRoute ) { }
+
+    ngOnInit() {
+        this.rooterInfo.params.subscribe( ( params: Params ) => this.productId = params["id"] );
+        // this.productId = this.rooterInfo.snapshot.params["id"];
+    }
+
+}
